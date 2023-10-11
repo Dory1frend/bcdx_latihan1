14abf80b677b623d84db5dbb781bbefd301af5f4
unknown <doryfrenda.com>
2023-10-11 20:34:18 +0700

    commit tanggal 11-20-2023

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
       title: "Latihan 1",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan 1"),
-          backgroundColor: Colors.amber,
+          title: Text("Latihan 1", style: TextStyle(color: Colors.white54),),
+          backgroundColor: Colors.green[400],
         ),
         body: Column(
           children: [
@@ -22,12 +22,132 @@ class MyApp extends StatelessWidget {
             Text("Hello world"),
             Text("Hello world"),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text("data 1"),
                 Text("data 2 "),
                 Text("data 3 "),
                 Text("data 4 "),
               ],
+            ),
+            Container(
+              width: 500,
+              height: 40,
+              color: Colors.grey[400],
+            ),
+            Row(
+              children: [
+                Container(
+              width: 30,
+              height: 30,
+              color: Colors.black,
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              color: Colors.black,
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              color: Colors.black,
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              color: Colors.black,
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              color: Colors.black,
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              color: Colors.black,
+              margin: EdgeInsets.all(20),
+            ),
+            Container(
+              width: 30,
+              height: 30,
+              color: Colors.black,
+              margin: EdgeInsets.all(20),
+            ),
+
+             ],
+            ),
+            Row(
+              children: [
+                Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+               ),
+              ),
+              Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+               ),
+              ),
+              Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+               ),
+              ),
+              Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+               ),
+              ),
+              Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+               ),
+              ),
+              Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+               ),
+              ),
+              Container(
+              width: 30,
+              height: 30,
+              margin: EdgeInsets.all(20),
+              decoration: const BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.circle,
+               ),
+              ),
+             ],
             ),
           ],
         ),
